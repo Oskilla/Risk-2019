cbb70f75041fcda90e809e9977dbd9588678c5da
haiti <haitiaya@gmail.com>
2019-11-02 23:04:22 +0100

    Creation of Spring boot project using MySQL Driver,JPA and H2 Database - PART2

---
+++ src/test/java/io/risk/riskgame/RiskgameApplicationTests.java
@@ -0,0 +1,13 @@
+package io.risk.riskgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
