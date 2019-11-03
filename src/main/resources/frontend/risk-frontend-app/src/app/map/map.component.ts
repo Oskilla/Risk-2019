785e3ba1cb0bc85e208fed377cca6384cb95e6ba
haiti <haitiaya@gmail.com>
2019-11-03 01:14:55 +0100

    The MAP of our risk game is integrated - PART2

---
+++ src/main/resources/frontend/risk-frontend-app/src/app/map/map.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+  constructor(private router: Router) {}
+}
