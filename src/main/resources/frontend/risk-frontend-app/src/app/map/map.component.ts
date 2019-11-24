35425b557f75fa399e7c5373ce50627e654eb8cf
haiti <haitiaya@gmail.com>
2019-11-24 10:07:15 +0100

    Initial reserves are set for official players.

--- src/main/resources/frontend/risk-frontend-app/src/app/map/map.component.ts
+++ src/main/resources/frontend/risk-frontend-app/src/app/map/map.component.ts
@@ -9,7 +9,7 @@ import {Router} from '@angular/router';
 export class MapComponent implements OnInit {
 
   constructor(private router: Router) {}
-
+  nbOfPlayers = 2;
   player = '';
   item1 = '';
   item2 = '';
@@ -17,6 +17,8 @@ export class MapComponent implements OnInit {
   item4 = '';
   item5 = '';
   item6 = '';
+  reserve = 0;
+
   ngOnInit() {
     this.item1 = localStorage.getItem('item1');
     this.item2 = localStorage.getItem('item2');
@@ -30,7 +32,6 @@ export class MapComponent implements OnInit {
       return '#00008B';
     }
     if (this.player === 'item2') {
-      // tslint:disable-next-line:no-unused-expression
       return '#9932CC';
     }
     if (this.player === 'item3') {
@@ -50,21 +51,71 @@ export class MapComponent implements OnInit {
   getPlayer3Color() {
     if (this.item3 === '') {
       return '#778899';
+    } else {
+      this.nbOfPlayers = this.nbOfPlayers + 1;
     }
   }
   getPlayer6Color() {
     if (this.item6 === '') {
       return '#778899';
+    } else {
+      this.nbOfPlayers = this.nbOfPlayers + 1;
     }
   }
   getPlayer4Color() {
     if (this.item4 === '') {
       return '#778899';
+    } else {
+      this.nbOfPlayers = this.nbOfPlayers + 1;
     }
   }
+
   getPlayer5Color() {
     if (this.item5 === '') {
       return '#778899';
+    } else {
+      this.nbOfPlayers = this.nbOfPlayers + 1;
+    }
+  }
+
+  setReserves() {
+    if (this.nbOfPlayers === 2) {
+      this.reserve = 40;
+    }
+    if (this.nbOfPlayers === 3) {
+      this.reserve = 35;
+    }
+    if (this.nbOfPlayers === 4) {
+      this.reserve = 30;
+    }
+    if (this.nbOfPlayers === 5) {
+      this.reserve = 25;
+    }
+    if (this.nbOfPlayers === 6) {
+      this.reserve = 20;
+    }
+  }
+
+  getDisplay(num: number) {
+    if (num === 3) {
+      if (this.item3 === '') {
+        return 'none';
+      }
+    }
+    if (num === 4) {
+      if (this.item4 === '') {
+        return 'none';
+      }
+    }
+    if (num === 5) {
+      if (this.item5 === '') {
+        return 'none';
+      }
+    }
+    if (num === 6) {
+      if (this.item6 === '') {
+        return 'none';
+      }
     }
   }
 }
