9bf6d2b0e2001421d4b0afe2b40da08d78e3d2ce
Axel <cousseau.axel@gmail.com>
2019-12-12 12:00:57 +0100

    init

--- src/main/resources/frontend/risk-frontend-app/src/app/app.component.ts
+++ src/main/resources/frontend/risk-frontend-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as SockJS from 'sockjs-client';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +11,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'risk-frontend-app';
 }
+
+
