5cb9d2ab6a3f391b609356f5e3caae5bc5104a88
haiti <haitiaya@gmail.com>
2019-12-27 04:36:01 +0100

    Added the Web Service in Controller directory to save the players.

---
+++ src/main/java/io/risk/riskgame/services/StorePlayersServiceImpl.java
@@ -0,0 +1,16 @@
+package io.risk.riskgame.services;
+
+import io.risk.riskgame.services.io_players.StorePlayersServiceInput;
+import io.risk.riskgame.services.io_players.StorePlayersServiceOutput;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class StorePlayersServiceImpl implements StorePlayersService {
+
+    @Override
+    public void savePlayersAsOneGame(StorePlayersServiceInput storePlayersServiceInput) {
+
+    }
+
+}
