1ea1fe794cb4c68c3bfe6b45f63a650a81fd2010
haiti <haitiaya@gmail.com>
2019-12-29 17:16:00 +0100

    Added the web service's Junit Testings.

---
+++ src/main/java/io/risk/riskgame/controllers/StorePlayersControllerTest.java
@@ -0,0 +1,56 @@
+package io.risk.riskgame.controllers;
+
+
+import io.risk.riskgame.controllers.io_players.StorePlayersControllerInput;
+import io.risk.riskgame.controllers.io_players.StorePlayersControllerOutput;
+import io.risk.riskgame.services.StorePlayersService;
+import io.risk.riskgame.services.io_players.StorePlayersServiceInput;
+import io.risk.riskgame.services.io_players.StorePlayersServiceOutput;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(value = StorePlayersController.class)
+public class StorePlayersControllerTest {
+
+    @MockBean
+    StorePlayersService storePlayersService;
+
+    private MockMvc mockMvc;
+
+    @Test
+    public void storePlayers () throws Exception {
+       StorePlayersServiceInput mockStorePlayersServiceInput = new StorePlayersServiceInput(
+                "playerA",
+                "blue",
+                "28",
+                "You Should conquer Africa And Asia",
+                "Egypt, Indonesia, Japan",
+                "220vhde-5G9HJHhgd8",
+                "false"
+        );
+       StorePlayersServiceOutput mockStorePlayersServiceOutput = new StorePlayersServiceOutput("added");
+        Mockito.when( storePlayersService.savePlayersAsOneGame(
+                mockStorePlayersServiceInput
+        )).thenReturn(mockStorePlayersServiceOutput);
+
+        String jsonRequestObject = "{\"name\":\"playerA\",\"color\":\"blue\",\"reserve\":\"28\",\"mission\":\"You should conquer Africa And Asia\",\"countries\":\"Egypt, Indonesia, Japan\",\"uuid\":\"220vhde-5G9HJHhgd8\",\"winner\":\"false\"}";
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/players/store")
+                .accept(MediaType.APPLICATION_JSON).content(jsonRequestObject)
+                .contentType(MediaType.APPLICATION_JSON);
+        String jsonResponseObject ="{\"added\":\"added\"}";
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        assert response.equals(jsonResponseObject);
+
+    }
+
+}
